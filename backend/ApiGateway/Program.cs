3584ca6720075c89d25b257f1028a521bcd66693
Estiven Chiluisa V. <echiluisa0257@uta.edu.ec>
2026-09-22 07:12:16 -0500

    feat: configurar ApiGateway con YARP

--- backend/ApiGateway/Program.cs
+++ backend/ApiGateway/Program.cs
@@ -1,12 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Agrega el servicio de YARP y lee la configuración del appsettings.json
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
 var app = builder.Build();
 
-// Mapea el enrutador para que intercepte las peticiones
 app.MapReverseProxy();
 
 app.Run();
