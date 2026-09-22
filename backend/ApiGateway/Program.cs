ffa0d20e81e90618a4933809fa762877d5ca5a21
Estiven Chiluisa V. <echiluisa0257@uta.edu.ec>
2026-09-21 23:59:39 -0500

    feat: configurar YARP en API Gateway con rutas para AuthService y CreditService

--- backend/ApiGateway/Program.cs
+++ backend/ApiGateway/Program.cs
@@ -1,6 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// Agrega el servicio de YARP y lee la configuración del appsettings.json
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Mapea el enrutador para que intercepte las peticiones
+app.MapReverseProxy();
 
 app.Run();
